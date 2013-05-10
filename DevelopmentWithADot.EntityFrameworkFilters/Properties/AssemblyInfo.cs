3c027ef80510a4eb397201bbd593f53dac9a4e75
Ricardo Peres <rjperes@hotmail.com>
2013-05-10 23:59:48 +0100

    Fixed copyright message

--- DevelopmentWithADot.EntityFrameworkFilters/Properties/AssemblyInfo.cs
+++ DevelopmentWithADot.EntityFrameworkFilters/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("DevelopmentWithADot.EntityFrameworkFilters")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyCopyright("Copyright © Ricardo Peres 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
--- README.md
+++ README.md
@@ -2,3 +2,6 @@ DevelopmentWithADot.EntityFrameworkFilters
 ==========================================
 
 Entity Framework Code First Automatic Filters
+First discussed on http://weblogs.asp.net/ricardoperes/archive/2013/05/04/filter-collections-automatically-with-entity-framework-code-first.aspx
+
+Copyright Ricardo Peres 2013
